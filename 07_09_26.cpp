d99c81499468d74a4f7071b36e459c8daa37ea16
Aman Yadav <amanyadavchak@gmail.com>
2026-09-07 19:43:31 +0530

    Friend Function

---
+++ 07_09_26.cpp
@@ -0,0 +1,17 @@
+//Create a class employee with basic salary use a friend function to calculate the gross salary HR 20% of basics salary and 
+#include <iostream>
+using namespace std;
+class Student{
+    private:
+        int basicSalary:
+    public:
+        Employee(int salary){
+            basic salary = salary;
+            
+        }  
+}
+
+int main() {
+    
+    return 0;
+}
