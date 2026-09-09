72b78360ed8711f0cc1d0865493121e21e2d44c0
Aman Yadav <amanyadavchak@gmail.com>
2026-09-09 15:57:58 +0530

    Friend function

--- 07_09_26.cpp
+++ 07_09_26.cpp
@@ -1,17 +1,35 @@
 //Create a class employee with basic salary use a friend function to calculate the gross salary HR 20% of basics salary and 
 #include <iostream>
 using namespace std;
-class Student{
-    private:
-        int basicSalary:
-    public:
-        Employee(int salary){
-            basic salary = salary;
-            
-        }  
+
+class Employee
+{
+private:
+    int basicSalary;
+
+public:
+    Employee(int salary)
+    {
+        basicSalary = salary;
+    }
+
+    friend float grossSalary(Employee e);
+};
+
+float grossSalary(Employee e)
+{
+    float HR = 0.20 * e.basicSalary;
+    float gross = e.basicSalary + HR;
+    return gross;
 }
 
-int main() {
-    
+int main()
+{
+    Employee e(50000);
+
+    cout << "Gross Salary = " << grossSalary(e);
+
     return 0;
 }
+
+//reate two classes student class and sports class , store the academic mark in the student class and sport mark in sports class and these should be private.Use the frien function to calculate the total marks 
