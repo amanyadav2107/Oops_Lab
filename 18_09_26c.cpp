441206f85ba6e6057568c7c4a3a900e65605f6cd
Aman Yadav <amanyadavchak@gmail.com>
2026-09-18 13:18:39 +0530

    This operator

--- 18_09_26c.cpp
+++ 18_09_26c.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 using namespace std;
-class Student{
+class Student
+{
 public:
     int age;
     string name;
-    
-    Student(int age, string name){ //Same parameter ke wajh se garbage value milega....if name are same
-        this -> age = age;
-        this -> name = name;
+
+    Student(int age, string name)
+    {                      // Same parameter ke wajh se garbage value milega....if name are same
+        this->age = age;   // arrow operator is used to point a function
+        this->name = name; // this operator is used to objectify the member function
+        //it only works on non static function
     }
-    void display(){
-        cout<<"Age = "<<age<<",";
-        cout<<"Name = " <<name<<endl;
+    void display()
+    {
+        cout << "Age = " << age << ",";
+        cout << "Name = " << name << endl;
+        cout << this; //it print the address of current object;
     }
 };
 
-int main() {
+int main()
+{
     Student s1(38, "John");
     Student s2(49, " Steve");
     s1.display();
