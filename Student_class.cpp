79d1fabe67319ddb6ef8ca3e2a4359559f7d0bb4
Aman Yadav <amanyadavchak@gmail.com>
2026-09-22 07:45:38 +0530

    Class and Object

--- Student_class.cpp
+++ Student_class.cpp
@@ -18,7 +18,21 @@ public:
     // Parameterised Constructor
     Student(int id, int age, string name, int nos)
     {
-        cout << " Student Parameterised Constructor called" << endl;
+        cout << "  Student Parameterised Constructor called" << endl;
+        this->id =id;
+        this->age=age;
+        this->name=name;
+        this->nos=nos;
+    }
+
+    // Copy Constructor
+    Student(const Student &srcobj) //srcobj => A
+    {
+        cout << "  Student copy Constructor called" << endl;
+        this->id = srcobj.id;
+        this->age = srcobj.age;
+        this->name = srcobj.name;
+        this->nos = srcobj.nos;
     }
 
     // Behaviour /Methods/ Function
@@ -58,6 +72,25 @@ int main()
     // A.nos =6;
 
     // A.bunk();
-    Student A(1, 15, "Aman", 5);
+    // Student A(1, 15, "Aman", 5); //Stack allocation
+    // Student B(2, 14, "Amey",6 );
+    // Student C(3, 16, "Amu",4 );
+    // Student D(4, 17, "Amika",7);
+
+    // cout<<A.name<<" "<<A.age<<endl;
+    // A.bunk();
+    // B.Sleep();
+
+    //copy constructor
+    // Student C =A;
+    // Student C(A);
+    // cout<<C.name<<" "<<A.name<<endl;
+
+    //Dynamic allocation, or Student pointer
+    Student *A =new Student(1, 14, "babban", 7);
+    cout<< A->name <<endl;
+    cout<< A->age <<endl;
+    A->Study();
+    delete A; //Call delete by urself otherwise distructor cantn't called in dynamic memory allocation
     return 0;
 }
